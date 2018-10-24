30ed8bb8334f930a36440dea51d69a555012cbf8
nikhil <n.kamboj15@gmail.com>
2018-10-24 14:38:36 +0530

    working code with google signIn and tomtom api's

--- app/src/main/java/androidapp/com/smartcitygcadrivers/MainActivity.java
+++ app/src/main/java/androidapp/com/smartcitygcadrivers/MainActivity.java
@@ -1,13 +1,125 @@
 package androidapp.com.smartcitygcadrivers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private SignInButton googleSignInUpButton;
+    private FirebaseAuth mFirebaseAuth;
+    private GoogleSignInOptions mGoogleSignInOptions;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private static final int RC_SIGN_IN = 234;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        googleSignInUpButton = findViewById(R.id.google_sign_in_button);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mGoogleSignInOptions =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions);
+
+        googleSignInUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+
+    private void signIn() {
+        //getting the google signIn intent
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+
+        //starting the activity for result
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //if the requestCode is the Google Sign In code that we defined at starting
+        if (requestCode == RC_SIGN_IN) {
+
+            //Getting the GoogleSignIn Task
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                //Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                //authenticating with firebase
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("MainActivity", "firebaseAuthWithGoogle:" + acct.getId());
+
+        //getting the auth credential
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+
+        //Now using firebase we are signing in the user here
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("MainActivity", "signInWithCredential:success");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, user.getDisplayName() +
+                                    " Signed In ", Toast.LENGTH_SHORT).show();
+                            if (isRegistered()) {
+
+                            } else {
+
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("MainActivity", "signInWithCredential:failure",
+                                    task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+    
+                        }
+                    }
+                });
+    }
+
+    /***
+     * to check if user has registered his pickup details or not
+     * @return boolean
+     */
+    private boolean isRegistered() {
+       return false;
     }
 }
--- app/src/main/java/androidapp/com/smartcitygcadrivers/RouteActivity.java
+++ app/src/main/java/androidapp/com/smartcitygcadrivers/RouteActivity.java
@@ -2,12 +2,32 @@ package androidapp.com.smartcitygcadrivers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RouteActivity extends AppCompatActivity {
+
+    private Button routeButtonSlot1;
+    private Button routeButtonSlot2;
+    private Button routeButtonSlot3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
+        setRouteButtons();
+
+    }
+
+    private void setRouteButtons() {
+        routeButtonSlot1 = findViewById(R.id.route_slot1);
+        routeButtonSlot2 = findViewById(R.id.route_slot2);
+        routeButtonSlot3 = findViewById(R.id.route_slot3);
+    }
+
+    /***
+     * calls the api to get the route, if route not present yet we go a pop up saying so
+     */
+    private void getRoutes() {
+
     }
 }
